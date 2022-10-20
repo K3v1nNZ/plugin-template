4b3ed6476d8cb22ad16bdb944a3f432213cf3675
K3v1nNZ <icegamer37@gmail.com>
2022-10-20 13:46:36 +1300

    now?

--- src/commands/rick.tsx
+++ src/commands/rick.tsx
@@ -20,6 +20,7 @@ export const rickCommand: Command = {
     execute: async function (args, message) {
         var rickRollURL = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
         //sendReply(message?.channel.id ?? "0", "https://www.youtube.com/watch?v=dQw4w9WgXcQ");
-        Messages._sendMessage(message?.channel.id ?? "0", rickRollURL);
+        Messages._sendMessage(message?.channel.id ?? "0", "This is a test");
+        return { content: rickRollURL };
     }
 }
