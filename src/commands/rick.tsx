72209d2270549e8e5d45bf433400932d22f60520
K3v1nNZ <icegamer37@gmail.com>
2022-10-21 17:01:40 +1300

    gif go brr

--- src/commands/rick.tsx
+++ src/commands/rick.tsx
@@ -2,6 +2,7 @@ import { ApplicationCommandInputType, ApplicationCommandOptionType, ApplicationC
 import { REST } from "enmity/modules/common"
 import { sendReply } from "enmity/api/clyde";
 import { Messages } from "enmity/metro/common";
+import { Token } from "enmity/metro/common";
 
 export const rickCommand: Command = {
     id: "rick-command",
@@ -20,5 +21,35 @@ export const rickCommand: Command = {
     execute: async function (args, message) {
         var rickRollURL = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
         return { content: "<" + rickRollURL + ">" };
+    }
+}
+
+export const favouriteGifsCommand: Command = {
+    id: "favourite-gifs-command",
+
+    name: "favourite-gifs",
+    displayName: "favourite-gifs",
+
+    description: "Get a list of your favourite gifs",
+    displayDescription: "Get a list of your favourite gifs",
+
+    type: ApplicationCommandType.Chat,
+    inputType: ApplicationCommandInputType.BuiltInText,
+
+    //options: [],
+
+    execute: async function (args, message) {
+        var token = Token.getToken();
+        var response = fetch("https://discord.com/api/v9/users/@me/settings-proto/2", {
+            method: "GET",
+            credentials: "include",
+            headers: {
+                "authorization": token
+            }
+        })
+        .then(response => response.json())
+        .then(data => {
+            sendReply(message?.channel.id ?? "0", data);
+        })
     }
 }
