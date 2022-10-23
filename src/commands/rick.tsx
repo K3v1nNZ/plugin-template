886b8d5babfb81da618d3f6181c59ee115f82e81
K3v1nNZ <icegamer37@gmail.com>
2022-10-23 18:14:21 +1300

    now

--- src/commands/rick.tsx
+++ src/commands/rick.tsx
@@ -52,6 +52,6 @@ export const favouriteGifsCommand: Command = {
         let buff = new Buffer(res, 'base64');
         let text = buff.toString('ascii');
         var matches = text.match('^(?!mailto:)(?:(?:http|https|ftp)://)(?:\\S+(?::\\S*)?@)?(?:(?:(?:[1-9]\\d?|1\\d\\d|2[01]\\d|22[0-3])(?:\\.(?:1?\\d{1,2}|2[0-4]\\d|25[0-5])){2}(?:\\.(?:[0-9]\\d?|1\\d\\d|2[0-4]\\d|25[0-4]))|(?:(?:[a-z\\u00a1-\\uffff0-9]+-?)*[a-z\\u00a1-\\uffff0-9]+)(?:\\.(?:[a-z\\u00a1-\\uffff0-9]+-?)*[a-z\\u00a1-\\uffff0-9]+)*(?:\\.(?:[a-z\\u00a1-\\uffff]{2,})))|localhost)(?::\\d{2,5})?(?:(/|\\?|#)[^\\s]*)?$')!;
-        sendReply(message?.channel.id ?? "0", matches.join(" link "));
+        sendReply(message?.channel.id ?? "0", matches.join(" brrr"));
     }
 }
